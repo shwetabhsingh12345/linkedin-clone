ecd198404c08c34caa9fb169cdd64e70d0402c1d
shwetabhsingh12345 <shwetabhsingh12345@gmail.com>
2024-04-13 13:15:16 +0530

    Added jwt

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,26 @@
 const express = require('express')
-
+const passport = require('passport')
+const {ExtractJwt, JwtStrategy} = require('passport-jwt')
 const app = express()
 
-app.get('/', (req, res)=>{
-    res.json("HI")
-})
+let opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+opts.secretOrKey = 'shwetabh'
+passport.use(
+    new JwtStrategy(opts, function(jwt_payload, done){
+        User.findOne({_id: jwt_payload.identifier}, function(err, user){
+            if(err){
+                done(err, false)
+            }
+            if(user){
+                done(null, user)
+            }
+            else{
+                done(null, false)
+            }
+        })
+    }
+    )
+)
 
 app.listen(3000)
